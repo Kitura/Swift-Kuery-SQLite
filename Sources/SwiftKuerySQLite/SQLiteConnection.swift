20c191c90558e45cdc05af78281a026f9e408983
Eugenio Baglieri <baglieri.eugenio@gmail.com>
2018-05-13 01:05:52 +0200

    Added Int32 type support when binding parameter
    This commit resolve an “Unsupported type error” when a query contains an Int32 bindend parameter

--- Sources/SwiftKuerySQLite/SQLiteConnection.swift
+++ Sources/SwiftKuerySQLite/SQLiteConnection.swift
@@ -360,6 +360,8 @@ public class SQLiteConnection: Connection {
                 resultCode = sqlite3_bind_double(statement, index, Double(value))
             case let value as Double:
                 resultCode = sqlite3_bind_double(statement, index, value)
+            case let value as  Int32:
+                resultCode = sqlite3_bind_int(statement, index, value)
             case let value as Int:
                 resultCode = sqlite3_bind_int64(statement, index, Int64(value))
             case let value as Data:
