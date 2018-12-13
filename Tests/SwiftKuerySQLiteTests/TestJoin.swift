2e8d8311b1a7c77e635e86e5edec0de30c89e890
kilnerm <38245247+kilnerm@users.noreply.github.com>
2018-12-13 15:18:58 +0000

    Merge changes for asynchronous result API (#32)

--- Tests/SwiftKuerySQLiteTests/TestJoin.swift
+++ Tests/SwiftKuerySQLiteTests/TestJoin.swift
@@ -115,49 +115,60 @@ class TestJoin: XCTestCase {
                                                         XCTAssertNotNil(rows, "SELECT returned no rows")
                                                         let resultSet = result.asResultSet!
                                                         XCTAssertEqual(rows!.count, 4, "SELECT returned wrong number of rows: \(rows!.count) instead of 4")
-                                                        let titles = resultSet.titles
-                                                        XCTAssertEqual(titles[0], "a", "Wrong column name: \(titles[0]) instead of 'a'")
-                                                        XCTAssertEqual(titles[1], "b", "Wrong column name: \(titles[1]) instead of 'b'")
-                                                        XCTAssertEqual(titles[2], "c", "Wrong column name: \(titles[2]) instead of 'c'")
-                                                        XCTAssertEqual(titles[3], "b", "Wrong column name: \(titles[3]) instead of 'b'")
-                                                        XCTAssertEqual(rows![0][0]! as! String, "apricot", "Wrong value in row 0 column 0")
-                                                        XCTAssertEqual(rows![1][0]! as! String, "banana", "Wrong value in row 1 column 0")
-                                                        XCTAssertEqual(rows![2][0]! as! String, "apple", "Wrong value in row 2 column 0")
-                                                        XCTAssertEqual(rows![3][0]! as! String, "banana", "Wrong value in row 3 column 0")
-
-                                                        let t1 = myTable1.as("t1")
-                                                        let t2 = myTable2.as("t2")
-                                                        let t3 = myTable3.as("t3")
-                                                        let s2 = Select(from: t1)
-                                                            .join(t2)
-                                                            .using(t1.b)
-                                                            .join(t3)
-                                                            .on(t1.a == t3.d)
-                                                        executeQuery(query: s2, connection: connection) { result, rows in
-                                                            XCTAssertEqual(result.success, true, "SELECT failed")
-                                                            XCTAssertNotNil(result.asResultSet, "SELECT returned no rows")
-                                                            XCTAssertEqual(rows!.count, 2, "SELECT returned wrong number of rows: \(rows!.count) instead of 2")
+                                                        resultSet.getColumnTitles() { titles, error in
+                                                            guard let titles = titles else {
+                                                                XCTFail("No titles returned")
+                                                                return
+                                                            }
+                                                            XCTAssertEqual(titles[0], "a", "Wrong column name: \(titles[0]) instead of 'a'")
+                                                            XCTAssertEqual(titles[1], "b", "Wrong column name: \(titles[1]) instead of 'b'")
+                                                            XCTAssertEqual(titles[2], "c", "Wrong column name: \(titles[2]) instead of 'c'")
+                                                            XCTAssertEqual(titles[3], "b", "Wrong column name: \(titles[3]) instead of 'b'")
                                                             XCTAssertEqual(rows![0][0]! as! String, "apricot", "Wrong value in row 0 column 0")
-                                                            XCTAssertEqual(rows![1][0]! as! String, "apple", "Wrong value in row 1 column 0")
+                                                            XCTAssertEqual(rows![1][0]! as! String, "banana", "Wrong value in row 1 column 0")
+                                                            XCTAssertEqual(rows![2][0]! as! String, "apple", "Wrong value in row 2 column 0")
+                                                            XCTAssertEqual(rows![3][0]! as! String, "banana", "Wrong value in row 3 column 0")
 
-                                                            let s3 = Select(from: myTable1)
-                                                                .leftJoin(myTable2)
-                                                                .on(myTable1.a == myTable2.c)
-                                                            executeQuery(query: s3, connection: connection) { result, rows in
+                                                            let t1 = myTable1.as("t1")
+                                                            let t2 = myTable2.as("t2")
+                                                            let t3 = myTable3.as("t3")
+                                                            let s2 = Select(from: t1)
+                                                                .join(t2)
+                                                                .using(t1.b)
+                                                                .join(t3)
+                                                                .on(t1.a == t3.d)
+                                                            executeQuery(query: s2, connection: connection) { result, rows in
                                                                 XCTAssertEqual(result.success, true, "SELECT failed")
                                                                 XCTAssertNotNil(result.asResultSet, "SELECT returned no rows")
-                                                                XCTAssertEqual(rows!.count, 8, "SELECT returned wrong number of rows: \(rows!.count) instead of 8")
+                                                                XCTAssertEqual(rows!.count, 2, "SELECT returned wrong number of rows: \(rows!.count) instead of 2")
+                                                                XCTAssertEqual(rows![0][0]! as! String, "apricot", "Wrong value in row 0 column 0")
+                                                                XCTAssertEqual(rows![1][0]! as! String, "apple", "Wrong value in row 1 column 0")
 
-
-                                                                let s4 = Select(from: t1)
-                                                                    .naturalJoin(t2)
-                                                                executeQuery(query: s4, connection: connection) { result, rows in
+                                                                let s3 = Select(from: myTable1)
+                                                                    .leftJoin(myTable2)
+                                                                    .on(myTable1.a == myTable2.c)
+                                                                executeQuery(query: s3, connection: connection) { result, rows in
                                                                     XCTAssertEqual(result.success, true, "SELECT failed")
                                                                     XCTAssertNotNil(result.asResultSet, "SELECT returned no rows")
-                                                                    let resultSet = result.asResultSet!
-                                                                    XCTAssertEqual(rows!.count, 4, "SELECT returned wrong number of rows: \(rows!.count) instead of 4")
-                                                                    XCTAssertEqual(resultSet.titles.count, 3, "SELECT returned wrong number of columns: \(resultSet.titles.count) instead of 3")
-                                                                    expectation.fulfill()
+                                                                    XCTAssertEqual(rows!.count, 8, "SELECT returned wrong number of rows: \(rows!.count) instead of 8")
+
+
+                                                                    let s4 = Select(from: t1)
+                                                                        .naturalJoin(t2)
+                                                                    executeQuery(query: s4, connection: connection) { result, rows in
+                                                                        XCTAssertEqual(result.success, true, "SELECT failed")
+                                                                        XCTAssertNotNil(result.asResultSet, "SELECT returned no rows")
+                                                                        let resultSet = result.asResultSet!
+                                                                        XCTAssertEqual(rows!.count, 4, "SELECT returned wrong number of rows: \(rows!.count) instead of 4")
+                                                                        resultSet.getColumnTitles() { titles, error in
+                                                                            guard let titles = titles else {
+                                                                                XCTFail("No titles returned")
+                                                                                return
+                                                                            }
+                                                                            XCTAssertEqual(titles.count, 3, "SELECT returned wrong number of columns: \(titles.count) instead of 3")
+                                                                            expectation.fulfill()
+                                                                        }
+                                                                    }
                                                                 }
                                                             }
                                                         }
@@ -231,8 +242,14 @@ class TestJoin: XCTestCase {
                                                         XCTAssertNotNil(result.asResultSet, "SELECT returned no rows")
                                                         let resultSet = result.asResultSet!
                                                         XCTAssertEqual(rows!.count, 5, "SELECT returned wrong number of rows: \(rows!.count) instead of 5")
-                                                        XCTAssertEqual(resultSet.titles.count, 1, "SELECT returned wrong number of columns: \(resultSet.titles.count) instead of 1")
-                                                        expectation.fulfill()
+                                                        resultSet.getColumnTitles() { titles, error in
+                                                            guard let titles = titles else {
+                                                                XCTFail("No titles returned")
+                                                                return
+                                                            }
+                                                            XCTAssertEqual(titles.count, 1, "SELECT returned wrong number of columns: \(titles.count) instead of 1")
+                                                            expectation.fulfill()
+                                                        }
                                                     }
                                                 }
                                             }
